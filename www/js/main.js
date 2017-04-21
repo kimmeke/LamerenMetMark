cfc51bbad01392ecda065879d6d3b13556e6a13b
Kimberly Verhoeven <kimberly@MacBook-Pro-van-Kimberly.local>
2017-04-21 13:24:30 +0200

    Begin versie 1

---
+++ www/js/main.js
@@ -0,0 +1,5 @@
+/*document.getElementById('navigatie').firstChild.innerHTML="<h2> Home </h2>";
+document.getElementById('navigatie').childNodes[1].innerHTML="<h2> Vrienden </h2>";
+document.getElementById('navigatie').childNodes[2].innerHTML="<h2> Chatten </h2>";
+
+*/
